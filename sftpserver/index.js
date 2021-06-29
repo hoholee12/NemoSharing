2df67c6ae651fa2d1a6efdb8cbcef4cb2b1829b5
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2021-06-29 15:51:21 +0900

    Add SFTP test server

---
+++ sftpserver/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var fs = require('fs');
+
+var SFTPServer = require("node-sftp-server");
+
+const { exec } = require('child_process');
+
+var srv = new SFTPServer();
+
+srv.listen(8022);
+console.log("listening on 8022...");
+
+srv.on("connect", function(auth, info) {
+  if (auth.method !== 'password' || auth.username !== "nemoux" || auth.password !== "nemoux") {
+    return auth.reject(['password'],false);
+  }
+  var username = auth.username;
+  var password = auth.password;
+
+  return auth.accept(function(session) {
+    return session.on("writefile", function(path, readstream) {
+    var writestream;
+	var filename = path + new Date().getTime() + '.jpeg';
+	  writestream = fs.createWriteStream(filename);
+      readstream.on("end",function() {
+		console.log(filename);
+		exec("/opt/pkgs/nemo.image/exec -f " + filename + " -a nemo.image", (error, stdout, stderr) => {});
+		  
+	  });
+      return readstream.pipe(writestream);
+    });
+  });
+});
