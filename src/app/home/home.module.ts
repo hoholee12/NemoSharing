28e7945923a36beb73c07cb037b572d53db1bf71
hoholee12 <hoholee12@gmail.com>
2021-12-16 21:37:13 +0900

    x

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   imports: [
